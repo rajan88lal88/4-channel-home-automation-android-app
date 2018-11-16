02061be26b617b9da418c72494ff4de7cde48159
rajan88lal88 <rajan88lal88@gmail.com>
2018-11-16 17:28:10 +0530

    control upto 4 appliances using android app over bluetooth. You can add widgets of the switches on your home screen to make it mi=uch more easier to control the appliances
    This code used a 4 channel relay, HC05 bluetooth module and arduino pro mini. Warning: This schematic requires you to open the electric sockets and change their wiring.
    The best feature of this code is that it enables you to controll the appliances normally with switch too.
    
    Once the switch has been turned on from the app you can turn it off from the app as well as toggling the main switch twice.(if applaince is on from switch but you turned it off from app then to turn it back on from the switch you will have to toggle it twice).
    
    For the app I hace used taske app which you can fing on play store along with a plugin called 'Bluetooth Serial from tasker' which also you can fing on playstore.
    Once you have installed both the apps, you can directly restore my tasker back attached which will create 6 commands for turning on and off 3 relays. You can then add widgets for these tasks easily from home screen.
    
    Note: To restore the backup of tasker, place the backup file in the folder "/Tasker/configs/user/". Place the backup.xml file in 'user' folder.
    
    You will have to pair to the hc05 module and edit each task's bluetooth device to your hc05 module... currently it is sending the commands to my hc05 module so configure it for your module. For this go to each task> got to Bluetooth serial > tap the edit icon on top right next to configuratio> Tap show all bluetooth devices> Select your hc 05 module.
    
    Note: You can even change the command for each relay here.

---
+++ Home_automation_tasker_app/Home_automation_tasker_app.ino
@@ -0,0 +1,133 @@
+#define ledPin 10
+#define s1 5
+#define s2 4
+#define s3 3
+#define s4 2
+#define r1 10
+#define r2 11
+#define r3 12
+#define r4 13
+#define t1 9
+#define t2 8
+#define t3 7
+#define t4 6
+
+int s1_,s2_,s3_,s4_,t1_,t2_,t3_,t4_;
+int ps1=-1;
+int ps2=-1;
+int ps3=-1;
+int ps4=-1;
+int state = 0;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  pinMode(t1,INPUT_PULLUP);
+  pinMode(t2,INPUT_PULLUP);
+  pinMode(t3,INPUT_PULLUP);
+  pinMode(t4,INPUT_PULLUP);
+  pinMode(s1,INPUT_PULLUP);
+  pinMode(s2,INPUT_PULLUP);
+  pinMode(s3,INPUT_PULLUP);
+  pinMode(s4,INPUT_PULLUP);
+  pinMode(r1,OUTPUT);
+  pinMode(r2,OUTPUT);
+  pinMode(r3,OUTPUT);
+  pinMode(r4,OUTPUT);
+  Serial.begin(38400); // Default communication rate of the Bluetooth module
+}
+
+void loop() {
+
+  
+  if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+    state = Serial.read(); // Reads the data from the serial port
+
+  }
+ if (state == '1') {
+  digitalWrite(r1, LOW); // Turn LED OFF
+  Serial.println("S1: OFF"); // Send back, to the phone, the String "LED: ON"
+  state = 0;
+ }
+ else if (state == '2') {
+  digitalWrite(r1, HIGH);
+  Serial.println("S1: ON");;
+  state = 0;
+ } 
+ if (state == '3') {
+  digitalWrite(r2, LOW); // Turn LED OFF
+  Serial.println("S2: OFF"); // Send back, to the phone, the String "LED: ON"
+  state = 0;
+ }
+ else if (state == '4') {
+  digitalWrite(r2, HIGH);
+  Serial.println("S2: ON");;
+  state = 0;
+ } 
+ if (state == '5') {
+  digitalWrite(r3, LOW); // Turn LED OFF
+  Serial.println("S3: OFF"); // Send back, to the phone, the String "LED: ON"
+  state = 0;
+ }
+ else if (state == '6') {
+  digitalWrite(r3, HIGH);
+  Serial.println("S3: ON");;
+  state = 0;
+ } 
+ if (state == '7') {
+  digitalWrite(r4, LOW); // Turn LED OFF
+  Serial.println("S4: OFF"); // Send back, to the phone, the String "LED: ON"
+  state = 0;
+ }
+ else if (state == '8') {
+  digitalWrite(r4, HIGH);
+  Serial.println("S4: ON");;
+  state = 0;
+ } 
+ 
+  s1_=digitalRead(s1);
+  s2_=digitalRead(s2);
+  s3_=digitalRead(s3);
+  s4_=digitalRead(s4);
+  if(s1_!=ps1)
+  {
+      ps1=s1_;
+      digitalWrite(r1,ps1);
+  }
+  if(s2_!=ps2)
+  {
+      ps2=s2_;
+      digitalWrite(r2,ps2);
+  }
+  if(s3_!=ps3)
+  {
+      ps3=s3_;
+      digitalWrite(r3,ps3);
+  }
+  if(s4_!=ps4)
+  {
+      ps4=s4_;
+      digitalWrite(r4,ps4);
+  }
+  t1_=digitalRead(t1);
+  t2_=digitalRead(t2);
+  t3_=digitalRead(t3);
+  t4_=digitalRead(t4);
+  
+  Serial.print(t1_);
+  Serial.print("\t");
+  Serial.print(t2_);
+  Serial.print("\t");
+  Serial.print(t3_);
+  Serial.print("\t");
+  Serial.print(t4_);
+  Serial.print("\t\t");
+  Serial.print(s1_);
+  Serial.print("\t");
+  Serial.print(s2_);
+  Serial.print("\t");
+  Serial.print(s3_);
+  Serial.print("\t");
+  Serial.print(s4_);
+  Serial.print("\n");
+  
+}
